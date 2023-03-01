2d9abb1582a98ca842206071d100861b7d4f35dd
B4l4zs4k0s <akosbalazs@proton.me>
2023-03-01 13:00:27 +0100

    First commit for project creation.

---
+++ SweetSliceAPI/src/main/java/com/cake/sweetsliceapi/SweetSliceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cake.sweetsliceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetSliceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetSliceApiApplication.class, args);
+	}
+
+}
---
+++ SweetSliceAPI/src/test/java/com/cake/sweetsliceapi/SweetSliceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cake.sweetsliceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetSliceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
